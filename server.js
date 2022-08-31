d0fac49b755cdafa535a4096f01dfe206be16f44
Tuyen Vu <tuyenvu1108@gmail.com>
2022-08-31 09:57:29 -0500

    modify

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ db.sequelize.sync();
 //Connecting with database
 require("./app/routers/course.routers")(app)
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
