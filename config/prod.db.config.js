653ddf8d3d825dd879ee150c770ed48d17d24691
Tuyen Vu <tuyenvu1108@gmail.com>
2022-08-31 09:36:59 -0500

    Add folders

---
+++ config/prod.db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: 'project2.eaglesoftwareteam.com',
+    port: 3306,
+    USER: 't12022',
+    PASSWORD: 'cs@oc2022t1',
+    DB: 'p2-t1-2022',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
