7505eeb5ab72b2b530660af547b1ea1747d4818b
Tuyen Vu <tuyenvu1108@gmail.com>
2022-09-05 13:01:20 -0500

    backend done

--- config/db.config.js
+++ config/db.config.js
@@ -2,11 +2,7 @@ module.exports = {
     HOST: 'localhost',
     USER: 'root',
     PASSWORD: 'yourpassword',
-<<<<<<< HEAD
     DB: 'courses',
-=======
-    DB: 'course1',
->>>>>>> 68ff628e653c5245ea905e98173fa957fd563cc6
     dialect: 'mysql',
     pool: {
         max: 5,
